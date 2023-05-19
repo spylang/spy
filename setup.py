4874cdca63e0682f6e493da1d630841bc5cdda83
Antonio Cuni <anto.cuni@gmail.com>
2023-05-19 16:28:56 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="spylang",
+      author='Antonio Cuni',
+      author_email='anto.cuni@gmail.com',
+      url='https://github.com/spylang/spy',
+      packages=find_packages(),
+      zip_safe=False,
+      )
