d0679a9b6ef310e13d53e4317bee56720c844af1
Antonio Cuni <anto.cuni@gmail.com>
2023-05-24 11:58:13 +0200

    add types to vm.py

--- spy/vm/vm.py
+++ spy/vm/vm.py
@@ -2,40 +2,45 @@ import fixedint
 from spy.vm.objects import W_Object, W_Type, W_NoneType, W_i32
 
 class Builtins:
-    pass
+    w_object: W_Type
+    w_type: W_Type
+    w_i32: W_Type
+    w_None: W_NoneType
 
 class SPyVM:
     """
     A Virtual Machine to execute SPy code.
     """
 
+    builtins: Builtins
+
     def __init__(self):
         self.init_builtins()
 
-    def init_builtins(self):
+    def init_builtins(self) -> None:
         self.builtins = Builtins()
         self.builtins.w_object = W_Object._w
         self.builtins.w_type = W_Type._w
-        self.builtins.w_None = W_NoneType._w_singleton
         self.builtins.w_i32 = W_i32._w
+        self.builtins.w_None = W_NoneType._w_singleton
 
-    def w_dynamic_type(self, w_obj):
+    def w_dynamic_type(self, w_obj: W_Object) -> W_Type:
         assert isinstance(w_obj, W_Object)
         pyclass = type(w_obj)
         assert pyclass._w is not None
         return pyclass._w
 
-    def issubclass(self, w_sub, w_super):
+    def issubclass(self, w_sub: W_Type, w_super: W_Type) -> bool:
         assert isinstance(w_super, W_Type)
         assert isinstance(w_sub, W_Type)
         w_class = w_sub
         while w_class is not self.builtins.w_None:
             if w_class is w_super:
                 return True
-            w_class = w_class.w_base
+            w_class = w_class.w_base  # type:ignore
         return False
 
-    def wrap(self, value):
+    def wrap(self, value) -> W_Object:
         """
         Useful for tests: magic funtion which wraps the given inter-level object
         into the most appropriate app-level W_* object.
@@ -48,7 +53,7 @@ class SPyVM:
             return value._w
         raise Exception(f"Cannot wrap interp-level objects of type {value.__class__.__name__}")
 
-    def unwrap(self, w_value):
+    def unwrap(self, w_value: W_Object):
         """
         Useful for tests: magic funtion which wraps the given app-level w_ object
         into the most appropriate inter-level object. Opposite of wrap().
