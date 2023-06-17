73e18688889d9b715cc096bfcb8632dab68ba1c4
Antonio Cuni <anto.cuni@gmail.com>
2023-06-17 17:40:31 +0200

    add an utility to print a class hierarchy, just because

--- spy/util.py
+++ spy/util.py
@@ -1,3 +1,5 @@
+#-*- encoding: utf-8 -*-
+
 class MagicExtend:
     """
     Create a magic @extend decorator which allows to easily monkey patch
@@ -52,3 +54,25 @@ class MagicExtend:
             if key not in ('__dict__', '__doc__', '__module__', '__weakref__'):
                 setattr(cls, key, value)
         return cls
+
+
+def print_class_hierarchy(cls):
+    CROSS  = "├── "
+    BAR    = "│   "
+    CORNER = "└── "
+    SPACE  = "    "
+
+    def print_class(cls, prefix, indent, marker):
+        print(f'{prefix}{marker}{cls.__name__}')
+        prefix += indent
+        subclasses = cls.__subclasses__()
+        if subclasses:
+            for subcls in subclasses[:-1]:
+                print_class(subcls, prefix, indent=BAR, marker=CROSS)
+            print_class(subclasses[-1], prefix, indent=SPACE, marker=CORNER)
+
+    print_class(cls, prefix='', indent='', marker='')
+
+if __name__ == '__main__':
+    import ast as py_ast
+    print_class_hierarchy(py_ast.AST)
