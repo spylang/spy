2ddd40c5eb9fc3a4e27a8a9c01c29ff52cbe2024
Antonio Cuni <anto.cuni@gmail.com>
2023-06-21 18:31:43 +0200

    introduce a new and better way to test for errors

--- spy/tests/test_irgen.py
+++ spy/tests/test_irgen.py
@@ -3,12 +3,13 @@ import textwrap
 import pytest
 import spy.ast
 from spy.parser import Parser
-from spy.errors import SPyTypeError
+from spy.errors import SPyTypeError, SPyCompileError
 from spy.irgen.symtable import Symbol
 from spy.irgen.typechecker import TypeChecker
 from spy.irgen.modgen import ModuleGen
 from spy.vm.vm import SPyVM
 from spy.vm.function import W_FunctionType
+from spy.util import Color
 from spy.tests.support import CompilerTest
 
 class AnyLocClass:
@@ -19,6 +20,9 @@ ANYLOC: Any = AnyLocClass()
 class TestIRGen(CompilerTest):
 
     def compile(self, src: str, *, only_typecheck: bool = False):
+        """
+        Compile the given src code into a W_Module
+        """
         srcfile = self.write_source('test.py', src)
         self.p = Parser.from_filename(str(srcfile))
         self.mod = self.p.parse()
@@ -27,6 +31,37 @@ class TestIRGen(CompilerTest):
         if not only_typecheck:
             self.modgen = ModuleGen(self.vm, self.t, self.mod)
             return self.modgen.make_w_mod()
+
+    def expect_error(self, src: str) -> SPyCompileError:
+        """
+        Same as compile(), but expect that the compilation fails and return the
+        raised SPyCompileError.
+
+        Note, this RETURNS the error, it does not RAISE it.
+        Then, you can assert more details about the error by using .match().
+        """
+        with pytest.raises(SPyCompileError) as exc:
+            self.compile(src)
+        return exc.value
+
+    def match(self, err: SPyCompileError, *expected_msgs: str) -> bool:
+        """
+        Check whether all the given messages are present in the error, either as
+        the main message or in the annotations.
+        """
+        all_messages = [err.message] + [ann.message for ann in err.annotations]
+        for expected in expected_msgs:
+            if expected not in all_messages:
+                expected = Color.set('yellow', expected)
+                print('Error match failed!')
+                print('The following error message was expected but not found:')
+                print(f'  - {expected}')
+                print()
+                print('Captured error')
+                formatted_error = err.format(use_colors=True)
+                print(textwrap.indent(formatted_error, '    '))
+                pytest.fail(f'Error message not found: {expected}')
+        return True
 
     def get_funcdef(self, name: str) -> spy.ast.FuncDef:
         for decl in self.mod.decls:
@@ -79,11 +114,16 @@ class TestIRGen(CompilerTest):
             """)
 
     def test_wrong_return_type(self):
-        with pytest.raises(SPyTypeError, match='expected `str`, got `i32`'):
-            self.compile("""
-            def foo() -> str:
-                return 42
-            """)
+        err = self.expect_error("""
+        def foo() -> str:
+            return 42
+        """)
+        assert self.match(
+            err,
+            'mismatched types',
+            'expected `str`, got `i32`',
+            'expected `str` because of return type',
+        )
 
     def test_local_variables(self):
         w_mod = self.compile(
