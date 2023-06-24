ff5a42ee97defb7ba81e7f59968019393399cd5b
Antonio Cuni <anto.cuni@gmail.com>
2023-06-24 11:48:57 +0200

    rename these methods in the parser, they were flipped

--- spy/parser.py
+++ spy/parser.py
@@ -37,7 +37,7 @@ class Parser:
         py_mod = py_ast.parse(self.src)
         assert isinstance(py_mod, py_ast.Module)
         py_mod.compute_all_locs(self.filename)
-        return self.to_Module(py_mod)
+        return self.from_py_Module(py_mod)
 
     def error(self, primary: str, secondary: str, loc: Loc) -> NoReturn:
         raise SPyParseError.simple(primary, secondary, loc)
@@ -51,14 +51,14 @@ class Parser:
         self.error(f'not implemented yet: {reason}',
                    'this is not yet supported by SPy', node.loc)
 
-    def to_Module(self, py_mod: py_ast.Module) -> spy.ast.Module:
+    def from_py_Module(self, py_mod: py_ast.Module) -> spy.ast.Module:
         mod = spy.ast.Module(filename=self.filename, decls=[])
         for py_stmt in py_mod.body:
             if isinstance(py_stmt, py_ast.FunctionDef):
-                funcdef = self.to_FuncDef(py_stmt)
+                funcdef = self.from_py_FuncionDef(py_stmt)
                 mod.decls.append(funcdef)
             elif isinstance(py_stmt, py_ast.AnnAssign):
-                vardef = self.to_Stmt_AnnAssign(py_stmt)
+                vardef = self.from_py_stmt_AnnAssign(py_stmt)
                 globvar = spy.ast.GlobalVarDef(vardef)
                 mod.decls.append(globvar)
             else:
@@ -67,7 +67,7 @@ class Parser:
         #
         return mod
 
-    def to_FuncDef(self, py_funcdef: py_ast.FunctionDef) -> spy.ast.FuncDef:
+    def from_py_FuncionDef(self, py_funcdef: py_ast.FunctionDef) -> spy.ast.FuncDef:
         if py_funcdef.decorator_list:
             loc = py_funcdef.decorator_list[0].loc
             self.error('decorators are not supported yet',
@@ -75,7 +75,7 @@ class Parser:
         #
         loc = py_funcdef.loc
         name = py_funcdef.name
-        args = self.to_FuncArgs(py_funcdef.args)
+        args = self.from_py_arguments(py_funcdef.args)
         #
         py_returns = py_funcdef.returns
         if py_returns is None:
@@ -88,8 +88,8 @@ class Parser:
             )
             self.error('missing return type', '', func_loc)
         #
-        return_type = self.to_Expr(py_returns)
-        body = [self.to_Stmt(py_stmt) for py_stmt in py_funcdef.body]
+        return_type = self.from_py_expr(py_returns)
+        body = [self.from_py_stmt(py_stmt) for py_stmt in py_funcdef.body]
         return spy.ast.FuncDef(
             loc = py_funcdef.loc,
             name = py_funcdef.name,
@@ -98,7 +98,7 @@ class Parser:
             body = body,
         )
 
-    def to_FuncArgs(self, py_args: py_ast.arguments) -> list[spy.ast.FuncArg]:
+    def from_py_arguments(self, py_args: py_ast.arguments) -> list[spy.ast.FuncArg]:
         if py_args.vararg:
             self.error('*args is not supported yet',
                        'this is not supported', py_args.vararg.loc)
@@ -116,9 +116,9 @@ class Parser:
                        'this is not supported', py_args.kwonlyargs[0].loc)
         assert not py_args.kw_defaults
         #
-        return [self.to_FuncArg(py_arg) for py_arg in py_args.args]
+        return [self.from_py_arg(py_arg) for py_arg in py_args.args]
 
-    def to_FuncArg(self, py_arg: py_ast.arg) -> spy.ast.FuncArg:
+    def from_py_arg(self, py_arg: py_ast.arg) -> spy.ast.FuncArg:
         if py_arg.annotation is None:
             self.error(f"missing type for argument '{py_arg.arg}'",
                        'type is missing here', py_arg.loc)
@@ -126,30 +126,30 @@ class Parser:
         return spy.ast.FuncArg(
             loc = py_arg.loc,
             name = py_arg.arg,
-            type = self.to_Expr(py_arg.annotation),
+            type = self.from_py_expr(py_arg.annotation),
         )
 
     # ====== spy.ast.Stmt ======
 
-    def to_Stmt(self, py_node: py_ast.stmt) -> spy.ast.Stmt:
-        return magic_dispatch(self, 'to_Stmt', py_node)
+    def from_py_stmt(self, py_node: py_ast.stmt) -> spy.ast.Stmt:
+        return magic_dispatch(self, 'from_py_stmt', py_node)
 
-    to_Stmt_NotImplemented = unsupported
+    from_py_stmt_NotImplemented = unsupported
 
-    def to_Stmt_Pass(self, py_node: py_ast.Pass) -> spy.ast.Pass:
+    def from_py_stmt_Pass(self, py_node: py_ast.Pass) -> spy.ast.Pass:
         return spy.ast.Pass(py_node.loc)
 
-    def to_Stmt_Return(self, py_node: py_ast.Return) -> spy.ast.Return:
+    def from_py_stmt_Return(self, py_node: py_ast.Return) -> spy.ast.Return:
         # we make 'return' completely equivalent to 'return None' already
         # during parsing: this simplifies quite a bit the rest
         value: spy.ast.Expr
         if py_node.value is None:
             value = spy.ast.Name(py_node.loc, 'None')
         else:
-            value = self.to_Expr(py_node.value)
+            value = self.from_py_expr(py_node.value)
         return spy.ast.Return(py_node.loc, value)
 
-    def to_Stmt_AnnAssign(self, py_node: py_ast.AnnAssign) -> spy.ast.VarDef:
+    def from_py_stmt_AnnAssign(self, py_node: py_ast.AnnAssign) -> spy.ast.VarDef:
         if not py_node.simple:
             self.error(f"not supported: assignments targets with parentheses",
                        "this is not supported", py_node.target.loc)
@@ -160,11 +160,11 @@ class Parser:
         return spy.ast.VarDef(
             loc = py_node.loc,
             name = py_node.target.id,
-            type = self.to_Expr(py_node.annotation),
-            value = self.to_Expr(py_node.value)
+            type = self.from_py_expr(py_node.annotation),
+            value = self.from_py_expr(py_node.value)
         )
 
-    def to_Stmt_Assign(self, py_node: py_ast.Assign) -> spy.ast.Stmt:
+    def from_py_stmt_Assign(self, py_node: py_ast.Assign) -> spy.ast.Stmt:
         # Assign can be pretty complex: it can have multiple targets, and a
         # target can be a Tuple or List in case of unpacking. For now, we
         # support only the very simple case of single assign to a Name.
@@ -176,36 +176,36 @@ class Parser:
         return spy.ast.Assign(
             loc = py_node.loc,
             target = py_target.id,
-            value = self.to_Expr(py_node.value)
+            value = self.from_py_expr(py_node.value)
         )
 
 
     # ====== spy.ast.Expr ======
 
-    def to_Expr(self, py_node: py_ast.expr) -> spy.ast.Expr:
-        return magic_dispatch(self, 'to_Expr', py_node)
+    def from_py_expr(self, py_node: py_ast.expr) -> spy.ast.Expr:
+        return magic_dispatch(self, 'from_py_expr', py_node)
 
-    to_Expr_NotImplemented = unsupported
+    from_py_expr_NotImplemented = unsupported
 
-    def to_Expr_Name(self, py_node: py_ast.Name) -> spy.ast.Name:
+    def from_py_expr_Name(self, py_node: py_ast.Name) -> spy.ast.Name:
         return spy.ast.Name(py_node.loc, py_node.id)
 
-    def to_Expr_Constant(self, py_node: py_ast.Constant) -> spy.ast.Constant:
+    def from_py_expr_Constant(self, py_node: py_ast.Constant) -> spy.ast.Constant:
         assert py_node.kind is None  # I don't know what is 'kind' here
         return spy.ast.Constant(py_node.loc, py_node.value)
 
-    def to_Expr_Subscript(self, py_node: py_ast.Subscript) -> spy.ast.GetItem:
-        value = self.to_Expr(py_node.value)
-        index = self.to_Expr(py_node.slice)
+    def from_py_expr_Subscript(self, py_node: py_ast.Subscript) -> spy.ast.GetItem:
+        value = self.from_py_expr(py_node.value)
+        index = self.from_py_expr(py_node.slice)
         return spy.ast.GetItem(py_node.loc, value, index)
 
-    def to_Expr_List(self, py_node: py_ast.List) -> spy.ast.List:
-        items = [self.to_Expr(py_item) for py_item in py_node.elts]
+    def from_py_expr_List(self, py_node: py_ast.List) -> spy.ast.List:
+        items = [self.from_py_expr(py_item) for py_item in py_node.elts]
         return spy.ast.List(py_node.loc, items)
 
-    def to_Expr_BinOp(self, py_node: py_ast.BinOp) -> spy.ast.BinOp:
-        left = self.to_Expr(py_node.left)
-        right = self.to_Expr(py_node.right)
+    def from_py_expr_BinOp(self, py_node: py_ast.BinOp) -> spy.ast.BinOp:
+        left = self.from_py_expr(py_node.left)
+        right = self.from_py_expr(py_node.right)
         #
         # this is some magic to automatically find the correct spy.ast.*
         # class
