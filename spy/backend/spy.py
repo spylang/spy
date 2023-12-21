93cbe3fa0772848424722c1e8d5c62b910a52c00
Antonio Cuni <anto.cuni@gmail.com>
2023-12-21 23:44:26 +0100

    refactor

--- spy/backend/spy.py
+++ spy/backend/spy.py
@@ -13,53 +13,56 @@ class SPyBackend:
     def __init__(self, mod: ast.Module) -> None:
         self.mod = mod
         self.out = TextBuilder(use_colors=False)
+        self.w = self.out.w
+        self.wl = self.out.wl
 
     def build(self):
-        self.emit(self.mod)
+        for decl in self.mod.decls:
+            self.emit_decl(decl)
         return self.out.build()
 
-    def emit(self, node: ast.Node) -> None:
-        magic_dispatch(self, 'emit', node)
+    def emit_decl(self, decl: ast.Decl) -> None:
+        magic_dispatch(self, 'emit_decl', decl)
 
-    def emit_Module(self, mod: ast.Module) -> None:
-        for decl in mod.decls:
-            self.emit(decl)
+    def emit_stmt(self, stmt: ast.Stmt) -> None:
+        magic_dispatch(self, 'emit_stmt', stmt)
+
+    def gen_expr(self, expr: ast.Expr) -> str:
+        return magic_dispatch(self, 'gen_expr', expr)
 
     # declarations
 
-    def emit_GlobalFuncDef(self, decl: ast.GlobalFuncDef) -> None:
-        self.emit(decl.funcdef)
+    def emit_decl_GlobalFuncDef(self, decl: ast.GlobalFuncDef) -> None:
+        self.emit_stmt(decl.funcdef)
 
     # statements
 
-    def emit_FuncDef(self, funcdef: ast.FuncDef) -> None:
-        self.out.write(f'def {funcdef.name}(')
-        # argument list
-        for i, arg in enumerate(funcdef.args):
-            is_last = (i == len(funcdef.args) - 1)
-            self.out.write(f'{arg.name}: ')
-            self.emit(arg.type)
-            if not is_last:
-                self.out.write(', ')
-        # end argument list
-        self.out.write(') -> ')
-        self.emit(funcdef.return_type)
-        self.out.writeline(':')
+    def _format_arglist(self, args: list[ast.FuncArg]) -> str:
+        l = []
+        for arg in args:
+            t = self.gen_expr(arg.type)
+            l.append(f'{arg.name}: {t}')
+        return ', '.join(l)
+
+    def emit_stmt_FuncDef(self, funcdef: ast.FuncDef) -> None:
+        args = self._format_arglist(funcdef.args)
+        ret = self.gen_expr(funcdef.return_type)
+        self.wl(f'def {funcdef.name}({args}) -> {ret}:')
         with self.out.indent():
             for stmt in funcdef.body:
-                self.emit(stmt)
+                self.emit_stmt(stmt)
 
-    def emit_Pass(self, stmt: ast.Pass) -> None:
-        self.out.writeline('pass')
+    def emit_stmt_Pass(self, stmt: ast.Pass) -> None:
+        self.wl('pass')
 
-    def emit_Return(self, ret: ast.Return) -> None:
-        self.out.write('return ')
-        self.emit(ret.value)
+    def emit_stmt_Return(self, ret: ast.Return) -> None:
+        v = self.gen_expr(ret.value)
+        self.wl(f'return {v}')
 
     # expressions
 
-    def emit_Constant(self, const: ast.Constant) -> None:
-        self.out.write(repr(const.value))
+    def gen_expr_Constant(self, const: ast.Constant) -> str:
+        return repr(const.value)
 
-    def emit_Name(self, name: ast.Name) -> None:
-        self.out.write(name.id)
+    def gen_expr_Name(self, name: ast.Name) -> str:
+        return name.id
