7b4518e5c89d704e6a2e45eb7a5407403b90fdfb
Antonio Cuni <anto.cuni@gmail.com>
2024-01-01 00:46:14 +0100

    fix test_modules

--- spy/tests/compiler/test_modules.py
+++ spy/tests/compiler/test_modules.py
@@ -1,12 +1,9 @@
-# mypy: ignore-errors
-# ^^^ remove this line when we re-enable these tests
-
 import pytest
-from spy.tests.support import CompilerTest, skip_backends, no_backend
+from spy.tests.support import CompilerTest, only_interp, expect_errors
 
 class TestBasic(CompilerTest):
 
-    def test_import(self, legacy):
+    def test_import(self):
         mod = self.compile("""
         from builtins import abs as my_abs
 
@@ -16,27 +13,29 @@ class TestBasic(CompilerTest):
         #
         assert mod.foo(-20) == 20
 
-    def test_import_errors(self, legacy):
-        self.expect_errors(
-            """
+    def test_import_errors_1(self):
+        ctx = expect_errors(
+            'cannot import `builtins.aaa`',
+            ('attribute `aaa` does not exist in module `builtins`', 'aaa')
+        )
+        with ctx:
+            self.compile("""
             from builtins import aaa
-            """,
-            errors = [
-                'cannot import `builtins.aaa`',
-                'attribute `aaa` does not exist in module `builtins`'
-            ]
-        )
-        self.expect_errors(
-            """
-            from xxx import aaa
-            """,
-            errors = [
-                'cannot import `xxx.aaa`',
-                'module `xxx` does not exist'
-            ]
-        )
+            """)
 
-    def test_two_modules(self, legacy):
+    def test_import_errors_2(self):
+        ctx = expect_errors(
+            'cannot import `xxx.aaa`',
+            ('module `xxx` does not exist', 'from xxx import aaa'),
+        )
+        with ctx:
+            self.compile("""
+            from xxx import aaa
+            """)
+
+    # we need to implement multi-module compilation to C
+    @only_interp
+    def test_two_modules(self):
         self.write_file(
             "delta.spy",
             """
@@ -53,14 +52,11 @@ class TestBasic(CompilerTest):
                 return x + get_delta()
             """)
 
-        w_delta = self.vm.import_('delta', legacy=self._legacy)
-        w_main = self.vm.import_('main', legacy=self._legacy)
+        w_delta = self.vm.import_('delta')
+        w_main = self.vm.import_('main')
         if self.backend == 'interp':
             from spy.backend.interp import InterpModuleWrapper
             delta = InterpModuleWrapper(self.vm, w_delta)
             main = InterpModuleWrapper(self.vm, w_main)
             assert delta.get_delta() == 10
             assert main.inc(4) == 14
-        else:
-            # we need to implement multi-module compilation to C
-            assert False
