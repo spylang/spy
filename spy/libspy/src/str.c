30e04619bbdd51bcb792abf81ced923fb6c8c17f
Antonio Cuni <anto.cuni@gmail.com>
2024-01-20 18:30:40 +0100

    fix the str ops in the C backend

--- spy/libspy/src/str.c
+++ spy/libspy/src/str.c
@@ -1,7 +1,7 @@
 #include "spy.h"
 
 spy_Str *
-spy_StrAlloc(size_t length) {
+spy_str_alloc(size_t length) {
     size_t size = sizeof(spy_Str) + length;
     spy_Str *res = (spy_Str*)spy_GcAlloc(size).p;
     res->length = length;
@@ -9,9 +9,9 @@ spy_StrAlloc(size_t length) {
 }
 
 spy_Str *
-spy_StrAdd(spy_Str *a, spy_Str *b) {
+spy_str_add(spy_Str *a, spy_Str *b) {
     size_t l = a->length + b->length;
-    spy_Str *res = spy_StrAlloc(l);
+    spy_Str *res = spy_str_alloc(l);
     char *buf = (char*)res->utf8;
     memcpy(buf, a->utf8, a->length);
     memcpy(buf + a->length, b->utf8, b->length);
@@ -19,9 +19,9 @@ spy_StrAdd(spy_Str *a, spy_Str *b) {
 }
 
 spy_Str *
-spy_StrMul(spy_Str *a, int32_t b) {
+spy_str_mul(spy_Str *a, int32_t b) {
     size_t l = a->length * b;
-    spy_Str *res = spy_StrAlloc(l);
+    spy_Str *res = spy_str_alloc(l);
     char *buf = (char*)res->utf8;
     for(int i=0; i<b; i++) {
         memcpy(buf, a->utf8, a->length);
@@ -32,7 +32,7 @@ spy_StrMul(spy_Str *a, int32_t b) {
 
 
 spy_Str *
-spy_StrGetItem(spy_Str *s, int32_t i) {
+spy_str_getitem(spy_Str *s, int32_t i) {
     // XXX this is wrong: it should return a code point
     size_t l = s->length;
     if (i < 0) {
@@ -42,7 +42,7 @@ spy_StrGetItem(spy_Str *s, int32_t i) {
         spy_panic("string index out of bound");
         return NULL;
     }
-    spy_Str *res = spy_StrAlloc(1);
+    spy_Str *res = spy_str_alloc(1);
     char *buf = (char*)res->utf8;
     buf[0] = s->utf8[i];
     return res;
--- spy/vm/ops.py
+++ spy/vm/ops.py
@@ -63,19 +63,19 @@ def i32_mul(vm: 'SPyVM', w_a: W_i32, w_b: W_i32) -> W_i32:
 def str_add(vm: 'SPyVM', w_a: W_str, w_b: W_str) -> W_str:
     assert isinstance(w_a, W_str)
     assert isinstance(w_b, W_str)
-    ptr_c = vm.ll.call('spy_StrAdd', w_a.ptr, w_b.ptr)
+    ptr_c = vm.ll.call('spy_str_add', w_a.ptr, w_b.ptr)
     return W_str.from_ptr(vm, ptr_c)
 
 @signature('def(s: str, n: i32) -> str')
 def str_mul(vm: 'SPyVM', w_a: W_str, w_b: W_i32) -> W_str:
     assert isinstance(w_a, W_str)
     assert isinstance(w_b, W_i32)
-    ptr_c = vm.ll.call('spy_StrMul', w_a.ptr, w_b.value)
+    ptr_c = vm.ll.call('spy_str_mul', w_a.ptr, w_b.value)
     return W_str.from_ptr(vm, ptr_c)
 
 @signature('def(s: str, i: i32) -> str')
 def str_getitem(vm: 'SPyVM', w_s: W_str, w_i: W_i32) -> W_str:
     assert isinstance(w_s, W_str)
     assert isinstance(w_i, W_i32)
-    ptr_c = vm.ll.call('spy_StrGetItem', w_s.ptr, w_i.value)
+    ptr_c = vm.ll.call('spy_str_getitem', w_s.ptr, w_i.value)
     return W_str.from_ptr(vm, ptr_c)
