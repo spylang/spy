4a7ab68628645c9af6affc556ec4bddc2eef2078
Antonio Cuni <anto.cuni@gmail.com>
2024-01-25 14:17:44 +0100

    use multimethods for the remaining ops

--- spy/vm/modules/operator/binop.py
+++ spy/vm/modules/operator/binop.py
@@ -8,6 +8,9 @@ if TYPE_CHECKING:
 
 MM = MultiMethodTable()
 
+# i32 ops
+MM.register('+',  'i32', 'i32', OPS.w_i32_add)
+MM.register('*',  'i32', 'i32', OPS.w_i32_mul)
 MM.register('==', 'i32', 'i32', OPS.w_i32_eq)
 MM.register('!=', 'i32', 'i32', OPS.w_i32_ne)
 MM.register('<' , 'i32', 'i32', OPS.w_i32_lt)
@@ -15,29 +18,19 @@ MM.register('<=', 'i32', 'i32', OPS.w_i32_le)
 MM.register('>' , 'i32', 'i32', OPS.w_i32_gt)
 MM.register('>=', 'i32', 'i32', OPS.w_i32_ge)
 
+# str ops
+MM.register('+',  'str', 'str', OPS.w_str_add)
+MM.register('*',  'str', 'i32', OPS.w_str_mul)
+MM.register('[]', 'str', 'i32', OPS.w_str_getitem)
+
 # XXX these should be labeled as 'blue'
 @OPS.primitive('def(l: type, r: type) -> dynamic')
 def ADD(vm: 'SPyVM', w_ltype: W_Type, w_rtype: W_Type) -> W_Object:
-    if w_ltype is w_rtype is B.w_i32:
-        return OPS.w_i32_add
-    elif w_ltype is w_rtype is B.w_str:
-        return OPS.w_str_add
-    return B.w_NotImplemented
+    return MM.lookup('+', w_ltype, w_rtype)
 
 @OPS.primitive('def(l: type, r: type) -> dynamic')
 def MUL(vm: 'SPyVM', w_ltype: W_Type, w_rtype: W_Type) -> W_Object:
-    if w_ltype is w_rtype is B.w_i32:
-        return OPS.w_i32_mul
-    if w_ltype is B.w_str and w_rtype is B.w_i32:
-        return OPS.w_str_mul
-    return B.w_NotImplemented
-
-@OPS.primitive('def(l: type, r: type) -> dynamic')
-def GETITEM(vm: 'SPyVM', w_vtype: W_Type, w_itype: W_Type) -> W_Object:
-    if w_vtype is B.w_str and w_itype is B.w_i32:
-        return OPS.w_str_getitem
-    return B.w_NotImplemented
-
+    return MM.lookup('*', w_ltype, w_rtype)
 
 @OPS.primitive('def(l: type, r: type) -> dynamic')
 def EQ(vm: 'SPyVM', w_ltype: W_Type, w_rtype: W_Type) -> W_Object:
@@ -62,3 +55,7 @@ def GT(vm: 'SPyVM', w_ltype: W_Type, w_rtype: W_Type) -> W_Object:
 @OPS.primitive('def(l: type, r: type) -> dynamic')
 def GE(vm: 'SPyVM', w_ltype: W_Type, w_rtype: W_Type) -> W_Object:
     return MM.lookup('>=', w_ltype, w_rtype)
+
+@OPS.primitive('def(l: type, r: type) -> dynamic')
+def GETITEM(vm: 'SPyVM', w_vtype: W_Type, w_itype: W_Type) -> W_Object:
+    return MM.lookup('[]', w_vtype, w_itype)
