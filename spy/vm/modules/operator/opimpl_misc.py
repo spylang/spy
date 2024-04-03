84448a35b6d95e08ff7d3465c8313485df55dced
Antonio Cuni <anto.cuni@gmail.com>
2024-04-03 19:58:11 +0200

    kill this file

--- spy/vm/modules/operator/opimpl_misc.py
+++
@@ -1,8 +0,0 @@
-from typing import TYPE_CHECKING
-from spy.vm.b import B
-from spy.vm.object import W_Object, W_Void, W_Dynamic
-from spy.vm.str import W_Str
-from spy.vm.module import W_Module
-from . import OP
-if TYPE_CHECKING:
-    from spy.vm.vm import SPyVM
