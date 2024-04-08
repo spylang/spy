13b62fb3dc5ea4c5dac2b687e55e100c12d4623f
Antonio Cuni <anto.cuni@gmail.com>
2024-04-08 12:47:20 +0200

    reduce some code duplication

--- spy/vm/modules/operator/attrop.py
+++ spy/vm/modules/operator/attrop.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 from spy.fqn import FQN
 from spy.vm.b import B
 from spy.vm.object import W_Object, W_Type, W_Dynamic, W_Void
@@ -13,6 +13,9 @@ if TYPE_CHECKING:
     from spy.vm.vm import SPyVM
 
 
+OpKind = Literal['get', 'set']
+
+
 @OP.builtin
 def GETITEM(vm: 'SPyVM', w_vtype: W_Type, w_itype: W_Type) -> W_Dynamic:
     return MM.lookup('[]', w_vtype, w_itype)
@@ -25,20 +28,7 @@ def GETATTR(vm: 'SPyVM', w_type: W_Type, w_attr: W_Str) -> W_Dynamic:
     if w_type is B.w_dynamic:
         raise NotImplementedError("implement me")
     elif attr in pyclass.__spy_members__:
-
-        member = pyclass.__spy_members__[attr]
-        W_Class = pyclass
-        W_Value = member.w_type.pyclass
-
-        # XXX this is wrong because it uses the type name as the modname. We
-        # need to rethink how FQNs are computed
-        fqn = FQN(modname=w_type.name, attr=f"__get_{attr}__")
-        @spy_builtin(fqn)
-        def opimpl_get(vm: 'SPyVM', w_obj: W_Class, w_attr: W_Str) -> W_Value:
-            return getattr(w_obj, attr)
-
-        return vm.wrap(opimpl_get)
-
+        return opimpl_member('get', vm, w_type, attr)
     elif pyclass.has_meth_overriden('op_GETATTR'):
         return pyclass.op_GETATTR(vm, w_type, w_attr)
 
@@ -52,7 +42,6 @@ def GETATTR(vm: 'SPyVM', w_type: W_Type, w_attr: W_Str) -> W_Dynamic:
     return B.w_NotImplemented
 
 
-
 @OP.builtin
 def SETATTR(vm: 'SPyVM', w_type: W_Type, w_attr: W_Str,
             w_vtype: W_Type) -> W_Dynamic:
@@ -61,21 +50,7 @@ def SETATTR(vm: 'SPyVM', w_type: W_Type, w_attr: W_Str,
     if w_type is B.w_dynamic:
         return OP.w_dynamic_setattr
     elif attr in pyclass.__spy_members__:
-
-        member = pyclass.__spy_members__[attr]
-        W_Class = pyclass
-        W_Value = member.w_type.pyclass
-
-        # XXX this is wrong because it uses the type name as the modname. We
-        # need to rethink how FQNs are computed
-        fqn = FQN(modname=w_type.name, attr=f"__set_{attr}__")
-        @spy_builtin(fqn)
-        def opimpl_set(vm: 'SPyVM', w_obj: W_Class, w_attr: W_Str,
-                       w_val: W_Value) -> W_Void:
-            setattr(w_obj, attr, w_val)
-
-        return vm.wrap(opimpl_set)
-
+        return opimpl_member('set', vm, w_type, attr)
     elif pyclass.has_meth_overriden('op_SETATTR'):
         return pyclass.op_SETATTR(vm, w_type, w_attr, w_vtype)
 
@@ -87,3 +62,32 @@ def SETATTR(vm: 'SPyVM', w_type: W_Type, w_attr: W_Str,
         return w_opimpl
 
     return B.w_NotImplemented
+
+
+def opimpl_member(kind: OpKind, vm: 'SPyVM', w_type: W_Type,
+                  attr: str) -> W_Dynamic:
+    pyclass = w_type.pyclass
+    member = pyclass.__spy_members__[attr]
+    W_Class = pyclass
+    W_Value = member.w_type.pyclass
+
+    # XXX FQN is wrong because it uses the type name as the modname. We
+    # need to rethink how FQNs are computed
+
+    if kind == 'get':
+        fqn = FQN(modname=w_type.name, attr=f"__get_{attr}__")
+        @spy_builtin(fqn)
+        def opimpl_get(vm: 'SPyVM', w_obj: W_Class, w_attr: W_Str) -> W_Value:
+            return getattr(w_obj, attr)
+        return vm.wrap(opimpl_get)
+
+    elif kind == 'set':
+        fqn = FQN(modname=w_type.name, attr=f"__set_{attr}__")
+        @spy_builtin(fqn)
+        def opimpl_set(vm: 'SPyVM', w_obj: W_Class, w_attr: W_Str,
+                       w_val: W_Value) -> W_Void:
+            setattr(w_obj, attr, w_val)
+        return vm.wrap(opimpl_set)
+
+    else:
+        assert False, f'Invalid OpKind: {kind}'
