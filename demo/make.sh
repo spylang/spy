ebcd80c2406b337f7d60c7a5abe7369bd86b4fcb
Antonio Cuni <anto.cuni@gmail.com>
2024-05-11 11:21:20 +0200

    introduce raw_c_demo and add a way to wrap C callbacks

--- demo/make.sh
+++ demo/make.sh
@@ -4,6 +4,15 @@ LIBSPY="/home/antocuni/anaconda/spy/spy/libspy"
 
 emcc $1 \
     -I ${LIBSPY}/include/ \
+    raw_c_demo.c \
+    ${LIBSPY}/src/jsffi/jsffi.c \
+    -sEXPORTED_FUNCTIONS="['_main']" \
+    -sDEFAULT_LIBRARY_FUNCS_TO_INCLUDE='$dynCall' \
+    -o raw_c_demo.js
+
+
+emcc $1 \
+    -I ${LIBSPY}/include/ \
     demo.c \
     ${LIBSPY}/src/jsffi/jsffi.c \
     -sEXPORTED_FUNCTIONS="['_main']" \
