86a6d9d0b402a34792b549af209c1c19f3fad379
Antonio Cuni <anto.cuni@gmail.com>
2024-06-27 12:19:53 +0200

    add a test for compiling standalone scripts in native mode, and the print function

--- spy/tests/test___main__.py
+++ spy/tests/test___main__.py
@@ -1,6 +1,7 @@
 from typing import Any
 import re
 import textwrap
+from subprocess import getstatusoutput
 import pytest
 from typer.testing import CliRunner
 from spy.__main__ import app
@@ -36,6 +37,11 @@ class TestMain:
         self.foo_spy.write(textwrap.dedent("""
         def add(x: i32, y: i32) -> i32:
             return x + y
+        """))
+        self.main_spy = tmpdir.join('main.spy')
+        self.main_spy.write(textwrap.dedent("""
+        def main() -> void:
+            print("hello world")
         """))
 
     def run(self, *args: Any) -> Any:
@@ -74,9 +80,18 @@ class TestMain:
         csrc = foo_c.read()
         assert csrc.startswith('#include <spy.h>')
 
-    def test_build(self):
+    def test_build_wasm(self):
         res, stdout = self.run(self.foo_spy)
         foo_wasm = self.tmpdir.join('foo.wasm')
         assert foo_wasm.exists()
         wasm_bytes = foo_wasm.read_binary()
         assert wasm_bytes.startswith(b'\0asm')
+
+    def test_build_native(self):
+        res, stdout = self.run("--toolchain", "native", self.main_spy)
+        main_exe = self.tmpdir.join('main')
+        assert main_exe.exists()
+        status, out = getstatusoutput(str(main_exe))
+        assert status == 0
+        # NOTE: getstatusoutput automatically strips the trailing \n
+        assert out == 'hello world'
