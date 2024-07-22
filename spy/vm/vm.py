cef5e1302b913de6903aa1df4b5edb6924766a5c
Antonio Cuni <anto.cuni@gmail.com>
2024-07-22 16:35:42 +0200

    WIP

--- spy/vm/vm.py
+++ spy/vm/vm.py
@@ -20,6 +20,7 @@ from spy.vm.modules.builtins import BUILTINS
 from spy.vm.modules.operator import OPERATOR
 from spy.vm.modules.types import TYPES, W_TypeDef
 from spy.vm.modules.rawbuffer import RAW_BUFFER
+from spy.vm.modules.jsffi import JSFFI
 
 class SPyVM:
     """
@@ -48,6 +49,7 @@ class SPyVM:
         self.make_module(OPERATOR)   # operator::
         self.make_module(TYPES)      # types::
         self.make_module(RAW_BUFFER) # rawbuffer::
+        self.make_module(JSFFI)      # jsffi::
 
     def import_(self, modname: str) -> W_Module:
         from spy.irgen.irgen import make_w_mod_from_file
