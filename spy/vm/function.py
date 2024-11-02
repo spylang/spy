756bab2799df2b0216afc33c2917e2e1b4e815f7
Antonio Cuni <anto.cuni@gmail.com>
2024-11-02 23:14:49 +0100

    remove pdb

--- spy/vm/function.py
+++ spy/vm/function.py
@@ -177,8 +177,6 @@ class W_ASTFunc(W_Func):
                  ) -> None:
         self.w_functype = w_functype
         self.qn = qn
-        if str(qn) == 'test::None':
-            import pdb;pdb.set_trace()
         self.funcdef = funcdef
         self.closure = closure
         self.locals_types_w = locals_types_w
