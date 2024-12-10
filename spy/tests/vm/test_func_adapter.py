3024f0efa26437a919ff50548913313b76bb2ff6
Antonio Cuni <anto.cuni@gmail.com>
2024-12-10 12:21:31 +0100

    WIP: initial implementation for a FuncAdapter

---
+++ spy/tests/vm/test_func_adapter.py
@@ -0,0 +1,30 @@
+import pytest
+from spy.vm.vm import SPyVM
+from spy.vm.function import W_FuncType
+from spy.vm.func_adapter import W_FuncAdapter, ArgSpec
+from spy.vm.primitive import W_I32
+from spy.vm.str import W_Str
+from spy.vm.builtin import builtin_func
+
+
+@builtin_func('test')
+def w_repeat(vm: 'SPyVM', w_s: W_Str, w_n: W_I32) -> W_Str:
+    s = vm.unwrap_str(w_s)
+    n = vm.unwrap_i32(w_n)
+    return vm.wrap(s * int(n))  # type: ignore
+
+def test_repeat():
+    vm = SPyVM()
+    w_s = vm.call(w_repeat, [vm.wrap('ab '), vm.wrap(3)])
+    assert vm.unwrap_str(w_s) == 'ab ab ab '
+
+def test_shuffle_args():
+    vm = SPyVM()
+    w_functype = W_FuncType.parse('def(n: i32, s: str) -> str')
+    w_adapter = W_FuncAdapter(
+        w_functype,
+        w_repeat,
+        [ArgSpec.Arg(1), ArgSpec.Arg(0)]
+    )
+    w_s = vm.call(w_adapter, [vm.wrap(3), vm.wrap('ab ')])
+    assert vm.unwrap_str(w_s) == 'ab ab ab '
---
+++ spy/vm/func_adapter.py
@@ -0,0 +1,39 @@
+from typing import TYPE_CHECKING, Sequence
+from dataclasses import dataclass
+from spy.fqn import FQN
+from spy.vm.object import W_Object
+from spy.vm.function import W_Func, W_FuncType
+if TYPE_CHECKING:
+    from spy.vm.vm import SPyVM
+
+# simulate Algebraic Data Type
+class ArgSpec:
+    pass
+
+@dataclass
+class Arg(ArgSpec):
+    i: int
+
+ArgSpec.Arg = Arg
+
+class W_FuncAdapter(W_Func):
+    """
+    WRITE ME
+    """
+    fqn = FQN('builtins::__adapter__')
+
+
+    def __init__(self, w_functype: W_FuncType, w_func: W_Func,
+                 args: list[ArgSpec]) -> None:
+        self.w_functype = w_functype
+        self.w_func = w_func
+        self.args = args
+
+    def spy_call(self, vm: 'SPyVM', args_w: Sequence[W_Object]) -> W_Object:
+        real_args_w = []
+        for spec in self.args:
+            if isinstance(spec, Arg):
+                real_args_w.append(args_w[spec.i])
+            else:
+                assert False
+        return self.w_func.spy_call(vm, real_args_w)
