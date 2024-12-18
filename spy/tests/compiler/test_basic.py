73277934b794d0ca6cf74e594ee4762e40ee75e4
Antonio Cuni <anto.cuni@gmail.com>
2024-12-18 17:30:06 +0100

    passing this test is the goal of the branch

--- spy/tests/compiler/test_basic.py
+++ spy/tests/compiler/test_basic.py
@@ -850,3 +850,18 @@ class TestBasic(CompilerTest):
         params = w_foo.w_functype.params
         assert params[0].w_type is w_S
         assert params[1].w_type is w_ptr_S1 is w_ptr_S2
+
+    @pytest.mark.xfail(reason='FIXME')
+    @only_interp
+    def test_eager_blue_eval(self):
+        mod = self.compile("""
+        @blue
+        def bar() -> dynamic:
+            return 42
+
+        def foo() -> type:
+            x = bar()
+            return STATIC_TYPE(x)
+        """)
+        w_type = mod.foo(unwrap=False)
+        assert w_type is B.w_i32
