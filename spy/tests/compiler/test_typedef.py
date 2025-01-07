759bcf0ad0329264cec5f90230c2f0c355bcdcb9
Antonio Cuni <anto.cuni@gmail.com>
2025-01-07 22:31:50 +0100

    add a test that we want to pass

--- spy/tests/compiler/test_typedef.py
+++ spy/tests/compiler/test_typedef.py
@@ -7,5 +7,24 @@ from spy.vm.function import W_ASTFunc
 from spy.tests.support import (CompilerTest, skip_backends,  expect_errors,
                                only_interp)
 
-class TestTypeDef(CompilerTest):
-    pass
+class TestTypedef(CompilerTest):
+
+    @pytest.mark.xfail(reason="IMPLEMENT ME")
+    def test_from_and_to(self):
+        mod = self.compile("""
+        @typedef
+        class MyInt:
+            __inner__: i32
+
+        def box(i: i32) -> MyInt:
+            return MyInt.from_inner(i)
+
+        def unbox(m: MyInt) -> i32:
+            return m.__inner__
+
+        def call_unbox(i: i32) -> i32:
+            return unbox(box(i))
+
+        """)
+        assert mod.box(42).__inner__ == 42
+        assert mod.call_unbox(43) == 43
