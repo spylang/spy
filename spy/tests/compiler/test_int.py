864cce54a5b0dffd989cbec7cb27e29ff3b12c81
Antonio Cuni <anto.cuni@gmail.com>
2025-04-02 22:36:38 +0200

    port the other tests

--- spy/tests/compiler/test_int.py
+++ spy/tests/compiler/test_int.py
@@ -12,6 +12,21 @@ def int_type(request):
 
 class TestInt(CompilerTest):
 
+    def test_i8_conversion(self):
+        mod = self.compile(
+        """
+        def foo(x: i32) -> i8:
+            return x
+
+        def bar(x: i8) -> i32:
+            return x
+        """)
+        assert mod.foo(42) == 42
+        assert mod.foo(128) == -128
+        assert mod.bar(42) == 42
+        assert mod.bar(-1) == -1
+        assert mod.bar(128) == -128
+
     def test_ops(self, int_type):
         mod = self.compile(f"""
         T = {int_type}
@@ -31,16 +46,17 @@ class TestInt(CompilerTest):
         assert mod.floordiv(11, 2) == 5
         assert mod.neg(-5) == 5
 
-    def test_i32_bitwise(self):
-        mod = self.compile("""
-        def shl(x: i32, y: i32) -> i32: return x << y
-        def shr(x: i32, y: i32) -> i32: return x >> y
-        def b_and(x: i32, y: i32) -> i32: return x & y
-        def b_or(x: i32, y: i32) -> i32: return x | y
-        def b_xor(x: i32, y: i32) -> i32: return x ^ y
+    def test_bitwise(self, int_type):
+        mod = self.compile(f"""
+        T = {int_type}
+        def shl(x: T, y: T) -> T:   return x << y
+        def shr(x: T, y: T) -> T:   return x >> y
+        def b_and(x: T, y: T) -> T: return x & y
+        def b_or(x: T, y: T) -> T:  return x | y
+        def b_xor(x: T, y: T) -> T: return x ^ y
         """)
-        assert mod.shl(128, 4) == 128 << 4
-        assert mod.shr(128, 4) == 128 >> 4
+        assert mod.shl(2, 5) == 2 << 5
+        assert mod.shr(32, 5) == 32 >> 5
         assert mod.b_and(7, 3) == 7 & 3
         assert mod.b_and(127, 7) == 127 & 7
         assert mod.b_or(127, 123) == 127 | 123
@@ -48,14 +64,15 @@ class TestInt(CompilerTest):
         assert mod.b_xor(16, 15) == 16 ^ 15
         assert mod.b_xor(16, 0) == 16 ^ 0
 
-    def test_i32_cmp(self):
-        mod = self.compile("""
-        def cmp_eq (x: i32, y: i32) -> bool: return x == y
-        def cmp_neq(x: i32, y: i32) -> bool: return x != y
-        def cmp_lt (x: i32, y: i32) -> bool: return x  < y
-        def cmp_lte(x: i32, y: i32) -> bool: return x <= y
-        def cmp_gt (x: i32, y: i32) -> bool: return x  > y
-        def cmp_gte(x: i32, y: i32) -> bool: return x >= y
+    def test_cmp(self, int_type):
+        mod = self.compile(f"""
+        T = {int_type}
+        def cmp_eq (x: T, y: T) -> bool: return x == y
+        def cmp_neq(x: T, y: T) -> bool: return x != y
+        def cmp_lt (x: T, y: T) -> bool: return x  < y
+        def cmp_lte(x: T, y: T) -> bool: return x <= y
+        def cmp_gt (x: T, y: T) -> bool: return x  > y
+        def cmp_gte(x: T, y: T) -> bool: return x >= y
         """)
         assert mod.cmp_eq(5, 5) is True
         assert mod.cmp_eq(5, 6) is False
@@ -78,18 +95,3 @@ class TestInt(CompilerTest):
         assert mod.cmp_gte(5, 6) is False
         assert mod.cmp_gte(5, 5) is True
         assert mod.cmp_gte(6, 5) is True
-
-    def test_i8_simple(self):
-        mod = self.compile(
-        """
-        def foo(x: i32) -> i8:
-            return x
-
-        def bar(x: i8) -> i32:
-            return x
-        """)
-        assert mod.foo(42) == 42
-        assert mod.foo(128) == -128
-        assert mod.bar(42) == 42
-        assert mod.bar(-1) == -1
-        assert mod.bar(128) == -128
