98dee04b6f81dd44352a72668d4d8be591705261
Antonio Cuni <anto.cuni@gmail.com>
2025-04-04 21:55:37 +0200

    WIP: write a (failing) test which is the goal of the branch

--- spy/tests/compiler/operator/test_itemop.py
+++ spy/tests/compiler/operator/test_itemop.py
@@ -53,6 +53,48 @@ class W_MyClass(W_Object):
         return W_OpImpl(w_setitem)
 
 
+class W_2DArray(W_Object):
+    "Simple 2D array of fixed size 3x3"
+    W = 3
+    H = 3
+
+    def __init__(self) -> None:
+        self.data = [0] * (self.W * self.H)
+
+    @builtin_method('__new__')
+    @staticmethod
+    def w_new(vm: 'SPyVM') -> 'W_2DArray':
+        return W_2DArray()
+
+    @builtin_method('__GETITEM__', color='blue')
+    @staticmethod
+    def w_GETITEM(vm: 'SPyVM', wop_self: W_OpArg,
+                  wop_i: W_OpArg, wop_j: W_OpArg) -> W_OpImpl:
+        @builtin_func('ext')
+        def w_getitem(vm: 'SPyVM', w_self: W_2DArray,
+                      w_i: W_I32, w_j: W_I32) -> W_I32:
+            i = vm.unwrap_i32(w_i)
+            j = vm.unwrap_i32(w_j)
+            k = i * (j*self.W)
+            val = w_self.data[i]
+            return vm.wrap(val)
+        return W_OpImpl(w_getitem)
+
+    @builtin_method('__SETITEM__', color='blue')
+    @staticmethod
+    def w_SETITEM(vm: 'SPyVM', wop_self: W_OpArg, wop_i: W_OpArg,
+                  wop_j: W_OpArg, wop_v: W_OpArg) -> W_OpImpl:
+        @builtin_func('ext')
+        def w_setitem(vm: 'SPyVM', w_self: W_2DArray, w_i: W_I32,
+                      w_j: W_I32, w_v: W_I32) -> None:
+            i = vm.unwrap_i32(w_i)
+            j = vm.unwrap_i32(w_j)
+            v = vm.unwrap_i32(w_v)
+            k = i * (j*self.W)
+            w_self.data[k] = v
+        return W_OpImpl(w_setitem)
+
+
 
 @no_C
 class TestItemop(CompilerTest):
@@ -61,6 +103,7 @@ class TestItemop(CompilerTest):
     def setup_ext(self) -> None:
         EXT = ModuleRegistry('ext')
         EXT.builtin_type('MyClass')(W_MyClass)
+        EXT.builtin_type('W_2DArray')(W_2DArray)
         self.vm.make_module(EXT)
 
     def test_getitem(self):
@@ -89,3 +132,23 @@ class TestItemop(CompilerTest):
         mod = self.compile(src)
         assert mod.foo(10, 5, 42) == 42  # Setting and getting index 5
         assert mod.foo(20, 7, 100) == 100  # Setting and getting index 7
+
+    def test_2darray(self):
+        self.setup_ext()
+        src = """
+        from ext import W_2DArray
+
+        def set_and_get(i: i32, j: i32, val: i32) -> i32:
+            arr = W_2DArray()
+            arr[i, j] = val
+            return arr[i, j]
+
+        def get_default(i: i32, j: i32) -> i32:
+            arr = W_2DArray()
+            return arr[i, j]
+        """
+        mod = self.compile(src)
+        assert mod.set_and_get(1, 1, 42) == 42
+        assert mod.set_and_get(0, 2, 24) == 24
+        assert mod.set_and_get(2, 1, 99) == 99
+        assert mod.get_default(1, 2) == 0
