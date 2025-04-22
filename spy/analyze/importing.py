adf1019543e1d391c11a280d80e4b21d9abff44d
Antonio Cuni <anto.cuni@gmail.com>
2025-04-22 17:16:09 +0200

    WIP: add docstring

--- spy/analyze/importing.py
+++ spy/analyze/importing.py
@@ -11,15 +11,79 @@ if TYPE_CHECKING:
     from spy.vm.vm import SPyVM
     from spy.vm.module import W_Module
 
-MODULE = Union[py.path.local, 'W_Module', None]
+MODULE = Union[ast.Module, 'W_Module', None]
+
 
 class ImportAnalizyer:
-    # XXX explain
+    """
+    The set of modules needed and loaded by a SPy program is statically
+    determined ahead of time.
 
+    This is very different than Python where `import` is a statement which can
+    trigger dynamic loading of a new module. In SPy, `import` is just a
+    declaration which makes avaiable in the current module an entity which has
+    already been loaded.
+
+    NOTE: eventually, we might want to slightly tweak the rules to allow
+    "true" dynamic loading also in SPy, but for now we support only
+    fully-static loading.
+
+    The set of modules to load is determined by statically looking at all the
+    `import` statements present from the root module, and recursively visiting
+    all the modules found in that way, INCLUDING the `import`s which are not
+    top-level.
+
+    This basicaly creates a tree of imports. E.g.:
+
+        # main.spy
+        import aaa
+        import bbb
+
+        # aaa.spy
+        import a1
+        def foo():
+            import a2
+
+        # bbb.spy
+        import aaa
+        import b1
+        import b2
+
+
+    This gives us an import tree which looks like that:
+
+    +-- main
+        +-- aaa
+        |   +-- a1
+        |   +-- a2
+        +-- bbb
+            +-- aaa
+            +-- b1
+            +-- b2
+
+    The import order is determined by doing a *depth-first, post-order
+    traversal of the tree, with memoization*. In the example above, it
+    corresponds to:
+
+    - a1
+    - a2
+    - aaa
+    - b1
+    - b2
+    - bbb
+    - main
+
+    This more or less corresponds to what happens normally in Python when all
+    the import statements are at top-level and there are no circular
+    dependencies.
+
+    Circular dependencies are currently not supported, but will be in the
+    future.
+    """
     def __init__(self, vm: 'SPyVM', modname: str) -> None:
         self.vm = vm
         self.queue = deque([modname])
-        self.mods = {}
+        self.mods: dict[str, MODULE] = {}
 
     def analyze(self) -> None:
         while self.queue:
@@ -60,7 +124,10 @@ class ImportAnalizyer:
 
     def import_all(self) -> None:
         assert not self.queue, 'call .analyze() first'
-        all_mods = reversed(self.mods.items()) # XXX explain
+
+        # XXX: the following logic is broken and doesn't do what the class
+        # docstring says
+        all_mods = reversed(self.mods.items())
         for modname, mod in all_mods:
             if isinstance(mod, ast.Module):
                 self.import_one(modname, mod)
