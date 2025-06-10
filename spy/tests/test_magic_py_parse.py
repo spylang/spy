e8608e668fca869c599e2a85501e32945c09224c
PatrickRedStar <peterkurkin1@gmail.com>
2025-06-10 15:11:12 +0300

    add test

--- spy/tests/test_magic_py_parse.py
+++ spy/tests/test_magic_py_parse.py
@@ -1,6 +1,9 @@
 import textwrap
 from spy.magic_py_parse import magic_py_parse, preprocess
 from spy.ast_dump import dump
+from spy.parser import Parser
+from spy.tests.support import expect_errors
+
 
 def test_preprocess_plain():
     src1 = textwrap.dedent("""
@@ -8,6 +11,7 @@ def test_preprocess_plain():
     """)
     src2, var_locs = preprocess(src1)
     assert src1 == src2
+
 
 def test_preprocess_var():
     src1 = textwrap.dedent("""
@@ -21,6 +25,7 @@ def test_preprocess_var():
     """)
     assert src2 == expected
     assert len(var_locs) == 2
+
 
 def test_magic_py_parse():
     src = textwrap.dedent("""
@@ -49,3 +54,27 @@ def test_magic_py_parse():
     )
     """)
     assert dumped.strip() == expected.strip()
+
+
+def test_magic_py_parse_error(tmpdir):
+    src = "def main() -> void:\n    if 1:\n"
+    f = tmpdir.join("test.spy")
+    f.write(src)
+    parser = Parser(src, str(f))
+    with expect_errors(
+            "expected an indented block after 'if' statement on line 2",
+            ("", "    if 1:")
+    ):
+        parser.parse()
+
+
+def test_magic_py_parse_tabs(tmpdir):
+    src = "def main() -> void:\n        print('hello')\n\tprint('world')\n"
+    f = tmpdir.join("test.spy")
+    f.write(src)
+    parser = Parser(src, str(f))
+    with expect_errors(
+            "inconsistent use of tabs and spaces in indentation",
+            ("", "\tprint('world')")
+    ):
+        parser.parse()
