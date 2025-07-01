df22acb083e500bc1e85161b502549a26a3a69fd
Antonio Cuni <anto.cuni@gmail.com>
2025-07-01 15:29:12 +0200

    initial commit for array.spy

---
+++ spy/tests/stdlib/test_array.py
@@ -0,0 +1,17 @@
+from spy.tests.support import CompilerTest
+
+class TestArray(CompilerTest):
+
+    def test_array1_simple(self):
+        src = """
+        from array import array1
+
+        def test() -> int:
+            a = array1[int](3)
+            a[0] = 1
+            a[1] = 2
+            a[2] = 3
+            return a[0] + a[1] + a[2]
+        """
+        mod = self.compile(src)
+        assert mod.test() == 6
