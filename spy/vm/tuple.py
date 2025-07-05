dfc2c06868a0ac8ed694e3ad8e55640b9567421b
Antonio Cuni <anto.cuni@gmail.com>
2025-07-05 12:31:45 +0200

    implement tuple equality

--- spy/vm/tuple.py
+++ spy/vm/tuple.py
@@ -1,8 +1,9 @@
 from typing import TYPE_CHECKING, Any
 from spy.vm.b import B
-from spy.vm.primitive import W_I32, W_Dynamic
-from spy.vm.object import (W_Object)
+from spy.vm.primitive import W_I32, W_Dynamic, W_Bool
+from spy.vm.object import W_Object
 from spy.vm.builtin import builtin_method
+from spy.vm.opspec import W_OpSpec, W_MetaArg
 if TYPE_CHECKING:
     from spy.vm.vm import SPyVM
 
@@ -37,8 +38,30 @@ class W_Tuple(W_Object):
         # XXX bound check?
         return w_tup.items_w[i]
 
+
     @builtin_method('__len__')
     @staticmethod
     def w_len(vm: 'SPyVM', w_tup: 'W_Tuple') -> W_I32:
         n = len(w_tup.items_w)
         return vm.wrap(n)
+
+    @builtin_method('__eq__', color='blue', kind='metafunc')
+    @staticmethod
+    def w_EQ(vm: 'SPyVM', wam_l: W_MetaArg, wam_r: W_MetaArg) -> W_OpSpec:
+        w_ltype = wam_l.w_static_T
+        w_rtype = wam_r.w_static_T
+        if w_ltype is not w_rtype:
+            return W_OpSpec.NULL
+
+        @vm.register_builtin_func(W_Tuple._w.fqn)
+        def w_eq(vm: 'SPyVM', w_t1: W_Tuple, w_t2: W_Tuple) -> W_Bool:
+            items1_w = w_t1.items_w
+            items2_w = w_t2.items_w
+            if len(items1_w) != len(items2_w):
+                return B.w_False
+            for w_1, w_2 in zip(items1_w, items2_w):
+                if vm.is_False(vm.eq(w_1, w_2)):
+                    return B.w_False
+            return B.w_True
+
+        return W_OpSpec(w_eq)
