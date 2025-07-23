b38bd59380e5dfd0b4fc1b7a39d3f9e2bc430d15
Antonio Cuni <anto.cuni@gmail.com>
2025-07-23 21:25:41 +0200

    remove comments

--- spy/vm/modules/operator/opimpl_dynamic.py
+++ spy/vm/modules/operator/opimpl_dynamic.py
@@ -13,7 +13,6 @@ def _dynamic_op(vm: 'SPyVM', w_op: W_Func,
     wop_a = W_OpArg.from_w_obj(vm, w_a)
     wop_b = W_OpArg.from_w_obj(vm, w_b)
     w_opimpl = vm.call_OP(None, w_op, [wop_a, wop_b])
-    #return vm.fast_call(w_opimpl, [w_a, w_b])
     return w_opimpl.execute(vm, [w_a, w_b])
 
 @OP.builtin_func
@@ -56,7 +55,6 @@ def w_dynamic_setattr(vm: 'SPyVM', w_obj: W_Dynamic, w_attr: W_Str,
     wop_attr = W_OpArg.from_w_obj(vm, w_attr)
     wop_v = W_OpArg.from_w_obj(vm, w_value)
     w_opimpl = vm.call_OP(None, OP.w_SETATTR, [wop_obj, wop_attr, wop_v])
-    #return vm.fast_call(w_opimpl, [w_obj, w_attr, w_value])
     return w_opimpl.execute(vm, [w_obj, w_attr, w_value])
 
 @OP.builtin_func
@@ -65,7 +63,6 @@ def w_dynamic_getattr(vm: 'SPyVM', w_obj: W_Dynamic,
     wop_obj = W_OpArg.from_w_obj(vm, w_obj)
     wop_attr = W_OpArg.from_w_obj(vm, w_attr)
     w_opimpl = vm.call_OP(None, OP.w_GETATTR, [wop_obj, wop_attr])
-    #return vm.fast_call(w_opimpl, [w_obj, w_attr])
     return w_opimpl.execute(vm, [w_obj, w_attr])
 
 @OP.builtin_func
@@ -77,5 +74,4 @@ def w_dynamic_call(vm: 'SPyVM', w_obj: W_Dynamic,
         for i, w_x in enumerate(all_args_w)
     ]
     w_opimpl = vm.call_OP(None, OP.w_CALL, all_args_wop)
-    #return vm.fast_call(w_opimpl, all_args_w)
     return w_opimpl.execute(vm, all_args_w)
