27afee0cd00d32e3907bd6c6d3303e546fd475b4
Antonio Cuni <anto.cuni@gmail.com>
2025-09-29 16:42:14 +0200

    this test passes out of the box

--- spy/tests/compiler/test_struct.py
+++ spy/tests/compiler/test_struct.py
@@ -33,3 +33,21 @@ class TestStructOnStack(CompilerTest):
         mod = self.compile(src)
         assert mod.foo(3, 4) == 7
         assert mod.bar(5, 6) == 11
+
+    def test_pass_and_return(self):
+        src = """
+        @struct
+        class Point:
+            x: i32
+            y: i32
+
+        def move(p: Point, delta: i32) -> Point:
+            return Point(p.x + delta, p.y + delta)
+
+        def foo(x: i32, y: i32) -> i32:
+            p = Point(x, y)
+            p2 = move(p, 3)
+            return p2.x + p2.y
+        """
+        mod = self.compile(src)
+        assert mod.foo(1, 2) == (1+3) + (2+3)
