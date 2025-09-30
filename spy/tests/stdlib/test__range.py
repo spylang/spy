d0652abfb0256171fb74b20abdbb7586762df2f9
Antonio Cuni <anto.cuni@gmail.com>
2025-09-30 15:38:11 +0200

    implement the fast_iter protocol for range

--- spy/tests/stdlib/test__range.py
+++ spy/tests/stdlib/test__range.py
@@ -14,3 +14,35 @@ class TestRange(CompilerTest):
         r = mod.make_range()
         assert r.start == 0
         assert r.stop == 10
+
+    def test_fastiter(self):
+        src = """
+        from _range import range, range_iterator
+
+        def get_iter() -> range_iterator:
+            r = range(3)
+            return r.__fastiter__()
+
+        def next(it: range_iterator) -> range_iterator:
+            return it.__next__()
+
+        def cont(it: range_iterator) -> bool:
+            return it.__continue_iteration__()
+        """
+        mod = self.compile(src)
+        it = mod.get_iter()
+
+        assert it == (0, 3)
+        assert mod.cont(it) == True
+        it = mod.next(it)
+
+        assert it == (1, 3)
+        assert mod.cont(it) == True
+        it = mod.next(it)
+
+        assert it == (2, 3)
+        assert mod.cont(it) == True
+        it = mod.next(it)
+
+        assert it == (3, 3)
+        assert mod.cont(it) == False
