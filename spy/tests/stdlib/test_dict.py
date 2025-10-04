0ec3e69952f56050129fbc068a0b031a38273732
dpdani <git@danieleparmeggiani.me>
2025-10-04 15:48:15 +0200

    implement `__contains__`, `__eq__`, and `__fastiter__`

--- spy/tests/stdlib/test_dict.py
+++ spy/tests/stdlib/test_dict.py
@@ -122,3 +122,105 @@ class TestDict(CompilerTest):
         mod = self.compile(src)
         with pytest.raises(Exception, match="KeyError"):
             mod.test()
+
+    def test_fastiter(self):
+        src = """
+        from dict import dict
+
+        def test() -> int:
+            d = dict[i32, i32]()
+            d[10] = -1
+            d[20] = -1
+            d[30] = -1
+            it = d.__fastiter__()
+            total = 0
+            while it.__continue_iteration__():
+                total = total + it.__item__()
+                it = it.__next__()
+            return total
+        """
+        mod = self.compile(src)
+        assert mod.test() == 60
+
+    def test_for_loop(self):
+        src = """
+        from dict import dict
+
+        def test() -> int:
+            d = dict[i32, i32]()
+            d[1] = -1
+            d[2] = -1
+            d[3] = -1
+            d[4] = -1
+            total = 0
+            for x in d:
+                total = total + x
+            return total
+        """
+        mod = self.compile(src)
+        assert mod.test() == 10
+
+    def test_contains(self):
+        src = """
+        from dict import dict
+        
+        def test() -> bool:
+            d = dict[i32, i32]()
+            d[1] = 1
+            return d.__contains__(1)
+        """
+        mod = self.compile(src)
+        assert mod.test()
+
+    def test_equal(self):
+        src = """
+        from dict import dict
+        
+        def test_eq() -> bool:
+            d1 = dict[i32, i32]()
+            d1[1] = -1
+            d1[2] = -1
+            d1[3] = -1
+            d2 = dict[i32, i32]()
+            d2[1] = -1
+            d2[2] = -1
+            d2[3] = -1
+            return d1 == d2
+        
+        def test_neq_value() -> bool:
+            d1 = dict[i32, i32]()
+            d1[1] = -1
+            d1[2] = -1
+            d1[3] = -1
+            d2 = dict[i32, i32]()
+            d2[1] = -1
+            d2[2] = -1
+            d2[3] = 0
+            return d1 == d2
+        
+        def test_neq_missing_key() -> bool:
+            d1 = dict[i32, i32]()
+            d1[1] = -1
+            d1[2] = -1
+            d1[3] = -1
+            d2 = dict[i32, i32]()
+            d2[1] = -1
+            d2[2] = -1
+            return d1 == d2
+        
+        def test_neq_key() -> bool:
+            d1 = dict[i32, i32]()
+            d1[1] = -1
+            d1[2] = -1
+            d1[3] = -1
+            d2 = dict[i32, i32]()
+            d2[1] = -1
+            d2[2] = -1
+            d1[33] = -1
+            return d1 == d2
+        """
+        mod = self.compile(src)
+        assert mod.test_eq()
+        assert not mod.test_neq_value()
+        assert not mod.test_neq_missing_key()
+        assert not mod.test_neq_key()
