eb335a9b9226039c14e4f15b605f987fdd833695
Viriathus1 <49337323+Viriathus1@users.noreply.github.com>
2025-10-06 10:37:08 +1100

    fixed variable not allowed in type expression

--- spy/vm/modules/operator/opimpl_int.py
+++ spy/vm/modules/operator/opimpl_int.py
@@ -13,15 +13,14 @@ class W_IntLike(Protocol):
 
 def make_ops(T: str, pyclass: type[W_Object]) -> None:
     w_T = pyclass._w  # e.g. B.w_i32
-    WT = Annotated[W_IntLike, w_T]
 
-    def _binop(vm: 'SPyVM', w_a: WT, w_b: WT, fn: Any) -> Any:
+    def _binop(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T], fn: Any) -> Any:
         a = w_a.value
         b = w_b.value
         res = fn(a, b)
         return vm.wrap(res)
 
-    def _unary_op(vm: 'SPyVM', w_a: WT, fn: Any) -> Any:
+    def _unary_op(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], fn: Any) -> Any:
         a = w_a.value
         res = fn(a)
         return vm.wrap(res)
@@ -32,81 +31,81 @@ def make_ops(T: str, pyclass: type[W_Object]) -> None:
     #     ...
 
     @OP.builtin_func(f'{T}_add')
-    def w_add(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_add(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a + b)
 
     @OP.builtin_func(f'{T}_sub')
-    def w_sub(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_sub(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a - b)
 
     @OP.builtin_func(f'{T}_mul')
-    def w_mul(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_mul(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a * b)
 
     @OP.builtin_func(f'{T}_div')
-    def w_div(vm: 'SPyVM', w_a: WT, w_b: WT) -> W_F64:
+    def w_div(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> W_F64:
         if w_b.value == 0:
             raise SPyError("W_ZeroDivisionError", "division by zero")
         return _binop(vm, w_a, w_b, lambda a, b: a / b)
 
     @OP.builtin_func(f'{T}_floordiv')
-    def w_floordiv(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_floordiv(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         if w_b.value == 0:
             raise SPyError("W_ZeroDivisionError", "integer division or modulo by zero")
         return _binop(vm, w_a, w_b, lambda a, b: a // b)
 
     @OP.builtin_func(f'{T}_mod')
-    def w_mod(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_mod(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         if w_b.value == 0:
             raise SPyError("W_ZeroDivisionError", "integer modulo by zero")
         return _binop(vm, w_a, w_b, lambda a, b: a % b)
 
     @OP.builtin_func(f'{T}_lshift')
-    def w_lshift(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_lshift(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a << b)
 
     @OP.builtin_func(f'{T}_rshift')
-    def w_rshift(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_rshift(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a >> b)
 
     @OP.builtin_func(f'{T}_and')
-    def w_and(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_and(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a & b)
 
     @OP.builtin_func(f'{T}_or')
-    def w_or(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_or(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a | b)
 
     @OP.builtin_func(f'{T}_xor')
-    def w_xor(vm: 'SPyVM', w_a: WT, w_b: WT) -> WT:
+    def w_xor(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _binop(vm, w_a, w_b, lambda a, b: a ^ b)
 
     @OP.builtin_func(f'{T}_eq')
-    def w_eq(vm: 'SPyVM', w_a: WT, w_b: WT) -> W_Bool:
+    def w_eq(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> W_Bool:
         return _binop(vm, w_a, w_b, lambda a, b: a == b)
 
     @OP.builtin_func(f'{T}_ne')
-    def w_ne(vm: 'SPyVM', w_a: WT, w_b: WT) -> W_Bool:
+    def w_ne(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> W_Bool:
         return _binop(vm, w_a, w_b, lambda a, b: a != b)
 
     @OP.builtin_func(f'{T}_lt')
-    def w_lt(vm: 'SPyVM', w_a: WT, w_b: WT) -> W_Bool:
+    def w_lt(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> W_Bool:
         return _binop(vm, w_a, w_b, lambda a, b: a < b)
 
     @OP.builtin_func(f'{T}_le')
-    def w_le(vm: 'SPyVM', w_a: WT, w_b: WT) -> W_Bool:
+    def w_le(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> W_Bool:
         return _binop(vm, w_a, w_b, lambda a, b: a <= b)
 
     @OP.builtin_func(f'{T}_gt')
-    def w_gt(vm: 'SPyVM', w_a: WT, w_b: WT) -> W_Bool:
+    def w_gt(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> W_Bool:
         return _binop(vm, w_a, w_b, lambda a, b: a > b)
 
     @OP.builtin_func(f'{T}_ge')
-    def w_ge(vm: 'SPyVM', w_a: WT, w_b: WT) -> W_Bool:
+    def w_ge(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T], w_b: Annotated[W_IntLike, w_T]) -> W_Bool:
         return _binop(vm, w_a, w_b, lambda a, b: a >= b)
 
     @OP.builtin_func(f'{T}_neg')
-    def w_neg(vm: 'SPyVM', w_a: WT) -> WT:
+    def w_neg(vm: 'SPyVM', w_a: Annotated[W_IntLike, w_T]) -> Annotated[W_IntLike, w_T]:
         return _unary_op(vm, w_a, lambda a: -a)
 
 
