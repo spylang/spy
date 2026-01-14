a1a8a19354c1f86de15e8463cdf2deca5ecfbe5b
Viriathus1 <49337323+Viriathus1@users.noreply.github.com>
2026-01-14 14:52:34 +1100

    added multimethod table for div ops in unsafe module

--- spy/vm/modules/unsafe/div.py
+++ spy/vm/modules/unsafe/div.py
@@ -2,6 +2,7 @@ from typing import TYPE_CHECKING, Annotated, Any, Protocol
 
 from spy.errors import SPyError
 from spy.vm.b import B
+from spy.vm.modules.operator.multimethod import MultiMethodTable
 from spy.vm.opspec import W_MetaArg
 from spy.vm.primitive import W_F32, W_F64, W_I8, W_I32, W_U8, W_U32
 from spy.vm.w import W_Object, W_OpSpec, W_Type
@@ -14,79 +15,58 @@ if TYPE_CHECKING:
 
 @UNSAFE.builtin_func(color="blue", kind="metafunc")
 def w_unchecked_div(vm: "SPyVM", wam_l: W_MetaArg, wam_r: W_MetaArg) -> W_OpSpec:
-    w_T = meta_args_type(wam_l, wam_r)
-    match w_T:
-        case B.w_i8:
-            return W_OpSpec(UNSAFE.w_i8_unchecked_div)
-        case B.w_u8:
-            return W_OpSpec(UNSAFE.w_u8_unchecked_div)
-        case B.w_i32:
-            return W_OpSpec(UNSAFE.w_i32_unchecked_div)
-        case B.w_u32:
-            return W_OpSpec(UNSAFE.w_u32_unchecked_div)
-        case B.w_f64:
-            return W_OpSpec(UNSAFE.w_f64_unchecked_div)
-        case B.w_f32:
-            return W_OpSpec(UNSAFE.w_f32_unchecked_div)
-        case _:
-            raise SPyError(
-                "W_TypeError",
-                f"Unsupported type `{w_T.fqn.human_name}` for unchecked division operation",
-            )
+    if w_opspec := MM.get_binary_opspec("unchecked_div", wam_l, wam_r):
+        return w_opspec
+    else:
+        l_type = wam_l.w_static_T.fqn.human_name
+        r_type = wam_r.w_static_T.fqn.human_name
+
+        raise SPyError(
+            "W_TypeError",
+            f"Unsupported types `{l_type}` / `{r_type}` for unchecked division operation",
+        )
 
 
 @UNSAFE.builtin_func(color="blue", kind="metafunc")
 def w_unchecked_floordiv(vm: "SPyVM", wam_l: W_MetaArg, wam_r: W_MetaArg) -> W_OpSpec:
-    w_T = meta_args_type(wam_l, wam_r)
-    match w_T:
-        case B.w_i8:
-            return W_OpSpec(UNSAFE.w_i8_unchecked_floordiv)
-        case B.w_u8:
-            return W_OpSpec(UNSAFE.w_u8_unchecked_floordiv)
-        case B.w_i32:
-            return W_OpSpec(UNSAFE.w_i32_unchecked_floordiv)
-        case B.w_u32:
-            return W_OpSpec(UNSAFE.w_u32_unchecked_floordiv)
-        case B.w_f64:
-            return W_OpSpec(UNSAFE.w_f64_unchecked_floordiv)
-        case B.w_f32:
-            return W_OpSpec(UNSAFE.w_f32_unchecked_floordiv)
-        case _:
-            raise SPyError(
-                "W_TypeError",
-                f"Unsupported type `{w_T.fqn.human_name}` for unchecked floordiv operation",
-            )
+    if w_opspec := MM.get_binary_opspec("unchecked_floordiv", wam_l, wam_r):
+        return w_opspec
+    else:
+        l_type = wam_l.w_static_T.fqn.human_name
+        r_type = wam_r.w_static_T.fqn.human_name
+
+        raise SPyError(
+            "W_TypeError",
+            f"Unsupported types `{l_type}` // `{r_type}` for unchecked floordiv operation",
+        )
 
 
 @UNSAFE.builtin_func(color="blue", kind="metafunc")
 def w_unchecked_mod(vm: "SPyVM", wam_l: W_MetaArg, wam_r: W_MetaArg) -> W_OpSpec:
-    w_T = meta_args_type(wam_l, wam_r)
-    match w_T:
-        case B.w_i8:
-            return W_OpSpec(UNSAFE.w_i8_unchecked_mod)
-        case B.w_u8:
-            return W_OpSpec(UNSAFE.w_u8_unchecked_mod)
-        case B.w_i32:
-            return W_OpSpec(UNSAFE.w_i32_unchecked_mod)
-        case B.w_u32:
-            return W_OpSpec(UNSAFE.w_u32_unchecked_mod)
-        case B.w_f64:
-            return W_OpSpec(UNSAFE.w_f64_unchecked_mod)
-        case B.w_f32:
-            return W_OpSpec(UNSAFE.w_f32_unchecked_mod)
-        case _:
-            raise SPyError(
-                "W_TypeError",
-                f"Unsupported type `{w_T.fqn.human_name}` for unchecked modulus operation",
-            )
+    if w_opspec := MM.get_binary_opspec("unchecked_mod", wam_l, wam_r):
+        return w_opspec
+    else:
+        l_type = wam_l.w_static_T.fqn.human_name
+        r_type = wam_r.w_static_T.fqn.human_name
+
+        raise SPyError(
+            "W_TypeError",
+            f"Unsupported types `{l_type}` % `{r_type}` for unchecked modulus operation",
+        )
 
 
-def meta_args_type(wam_l: W_MetaArg, wam_r: W_MetaArg) -> W_Type:
-    if (wam_l.w_static_T == B.w_f64) ^ (wam_r.w_static_T == B.w_f64):
-        if wam_l.w_static_T != B.w_f64:
-            return wam_r.w_static_T
+@UNSAFE.builtin_func(color="blue", kind="metafunc")
+def w_ieee754_div(vm: "SPyVM", wam_l: W_MetaArg, wam_r: W_MetaArg) -> W_OpSpec:
+    if w_opspec := MM.get_binary_opspec("ieee754_div", wam_l, wam_r):
+        return w_opspec
+    else:
+        l_type = wam_l.w_static_T.fqn.human_name
+        r_type = wam_r.w_static_T.fqn.human_name
 
-    return wam_l.w_static_T
+        raise SPyError(
+            "W_TypeError",
+            f"Unsupported types `{l_type}` / `{r_type}` for unchecked ieee754 division operation",
+        )
 
 
 class W_NumLike(Protocol):
@@ -181,21 +161,6 @@ def w_f32_unchecked_mod(vm: "SPyVM", w_a: W_F32, w_b: W_F32) -> W_F32:
     return vm.wrap(res)
 
 
-@UNSAFE.builtin_func(color="blue", kind="metafunc")
-def w_ieee754_div(vm: "SPyVM", wam_l: W_MetaArg, wam_r: W_MetaArg) -> W_OpSpec:
-    w_T = meta_args_type(wam_l, wam_r)
-    match w_T:
-        case B.w_f64:
-            return W_OpSpec(UNSAFE.w_f64_ieee754_div)
-        case B.w_f32:
-            return W_OpSpec(UNSAFE.w_f32_ieee754_div)
-        case _:
-            raise SPyError(
-                "W_TypeError",
-                f"Unsupported type `{w_T.fqn.human_name}` for unchecked division operation",
-            )
-
-
 @UNSAFE.builtin_func
 def w_f64_ieee754_div(vm: "SPyVM", w_a: W_F64, w_b: W_F64) -> W_F64:
     a = w_a.value
@@ -220,3 +185,54 @@ def w_f32_ieee754_div(vm: "SPyVM", w_a: W_F32, w_b: W_F32) -> W_F32:
     b = vm.unwrap_f32(w_b)
     res = vm.ll.call("spy_unsafe$f32_ieee754_div", a, b)
     return vm.wrap(res)
+
+
+MM = MultiMethodTable()
+
+MM.register("unchecked_div", "i8", "i8", UNSAFE.w_i8_unchecked_div)
+MM.register("unchecked_div", "u8", "u8", UNSAFE.w_u8_unchecked_div)
+MM.register("unchecked_div", "i32", "i32", UNSAFE.w_i32_unchecked_div)
+MM.register("unchecked_div", "u32", "u32", UNSAFE.w_u32_unchecked_div)
+MM.register("unchecked_div", "f64", "f64", UNSAFE.w_f64_unchecked_div)
+MM.register("unchecked_div", "f32", "f32", UNSAFE.w_f32_unchecked_div)
+MM.register("unchecked_floordiv", "i8", "i8", UNSAFE.w_i8_unchecked_floordiv)
+MM.register("unchecked_floordiv", "u8", "u8", UNSAFE.w_u8_unchecked_floordiv)
+MM.register("unchecked_floordiv", "i32", "i32", UNSAFE.w_i32_unchecked_floordiv)
+MM.register("unchecked_floordiv", "u32", "u32", UNSAFE.w_u32_unchecked_floordiv)
+MM.register("unchecked_floordiv", "f64", "f64", UNSAFE.w_f64_unchecked_floordiv)
+MM.register("unchecked_floordiv", "f32", "f32", UNSAFE.w_f32_unchecked_floordiv)
+MM.register("unchecked_mod", "i8", "i8", UNSAFE.w_i8_unchecked_mod)
+MM.register("unchecked_mod", "u8", "u8", UNSAFE.w_u8_unchecked_mod)
+MM.register("unchecked_mod", "i32", "i32", UNSAFE.w_i32_unchecked_mod)
+MM.register("unchecked_mod", "u32", "u32", UNSAFE.w_u32_unchecked_mod)
+MM.register("unchecked_mod", "f64", "f64", UNSAFE.w_f64_unchecked_mod)
+MM.register("unchecked_mod", "f32", "f32", UNSAFE.w_f32_unchecked_mod)
+
+# float op combinations
+for num_t in ["i8", "u8", "i32", "f32"]:
+    MM.register("unchecked_div", "f64", num_t, UNSAFE.w_f64_unchecked_div)
+    MM.register("unchecked_div", num_t, "f64", UNSAFE.w_f64_unchecked_div)
+    MM.register("unchecked_floordiv", "f64", num_t, UNSAFE.w_f64_unchecked_floordiv)
+    MM.register("unchecked_floordiv", num_t, "f64", UNSAFE.w_f64_unchecked_floordiv)
+    MM.register("unchecked_mod", "f64", num_t, UNSAFE.w_f64_unchecked_mod)
+    MM.register("unchecked_mod", num_t, "f64", UNSAFE.w_f64_unchecked_mod)
+
+for int_t in ["i32"]:
+    MM.register("unchecked_div", "f32", int_t, UNSAFE.w_f32_unchecked_div)
+    MM.register("unchecked_div", int_t, "f32", UNSAFE.w_f32_unchecked_div)
+    MM.register("unchecked_floordiv", "f32", int_t, UNSAFE.w_f32_unchecked_floordiv)
+    MM.register("unchecked_floordiv", int_t, "f32", UNSAFE.w_f32_unchecked_floordiv)
+    MM.register("unchecked_mod", "f32", int_t, UNSAFE.w_f32_unchecked_mod)
+    MM.register("unchecked_mod", int_t, "f32", UNSAFE.w_f32_unchecked_mod)
+
+
+# ieee754_div ops
+MM.register("ieee754_div", "f64", "f64", UNSAFE.w_f64_ieee754_div)
+MM.register("ieee754_div", "i32", "i32", UNSAFE.w_f64_ieee754_div)
+MM.register("ieee754_div", "i32", "f64", UNSAFE.w_f64_ieee754_div)
+MM.register("ieee754_div", "f64", "i32", UNSAFE.w_f64_ieee754_div)
+MM.register("ieee754_div", "f32", "f64", UNSAFE.w_f64_ieee754_div)
+MM.register("ieee754_div", "f64", "f32", UNSAFE.w_f64_ieee754_div)
+MM.register("ieee754_div", "f32", "f32", UNSAFE.w_f32_ieee754_div)
+MM.register("ieee754_div", "f32", "i32", UNSAFE.w_f32_ieee754_div)
+MM.register("ieee754_div", "i32", "f32", UNSAFE.w_f32_ieee754_div)
