964ed0d05a9ab1338dc94cdd46a8e3df9184567c
Jeff Glass <glass.jeffrey@gmail.com>
2026-01-16 21:08:51 +0000

    Much less janky implementation of eval_Slice in astframe

--- spy/analyze/symtable.py
+++ spy/analyze/symtable.py
@@ -197,7 +197,6 @@ class SymTable:
 
         add_sym("range", None, ImportRef("_range", "range"))
         add_sym("list", None, ImportRef("_list", "list"))
-        add_sym("slice", None, ImportRef("_slice", "Slice"))
         add_sym("Slice", None, ImportRef("_slice", "Slice"))
         return scope
 
