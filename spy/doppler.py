9ddf446e4e7eefc2c1ef0b578115baa68b376b5a
Jeff Glass <glass.jeffrey@gmail.com>
2026-02-08 16:19:59 +0000

    Adjust formatting of doppler.py after merge

--- spy/doppler.py
+++ spy/doppler.py
@@ -494,6 +494,7 @@ class DopplerFrame(ASTFrame):
         v_stop = self.shifted_expr[op.stop]
         v_step = self.shifted_expr[op.step]
         return self.shift_opimpl(op, w_opimpl, [v_start, v_stop, v_step])
+
     def shift_expr_Dict(self, dict: ast.Dict, wam: W_MetaArg) -> ast.Expr:
         if len(dict.items) == 0:
             assert wam.w_static_T is SPY.w_EmptyDictType
