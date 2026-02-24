7162588cfd805e983cae050c392ab24228b69bc2
Antonio Cuni <anto.cuni@gmail.com>
2026-02-24 16:02:58 +0100

    implement tuple.__getitem__

--- spy/tests/stdlib/test__tuple.py
+++ spy/tests/stdlib/test__tuple.py
@@ -16,3 +16,15 @@ class TestTange(CompilerTest):
         assert p == ("hello", 42)
         assert p._item0 == "hello"
         assert p._item1 == 42
+
+    def test_getitem(self):
+        src = """
+        from _tuple import tuple
+
+        def foo(x: i32, y: i32) -> i32:
+            t = tuple[int, int](x, y)
+            return t[1]
+        """
+        mod = self.compile(src)
+        res = mod.foo(10, 20)
+        assert res == 20
