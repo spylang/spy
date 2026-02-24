411fb697324a374ae8e4fd7ea97b7c8a4a88a3ea
Viriathus1 <49337323+Viriathus1@users.noreply.github.com>
2026-02-24 14:44:31 +1100

    added spy_Complex128 type to backend

---
+++ spy/libspy/include/spy/complex.h
@@ -0,0 +1,11 @@
+#ifndef SPY_COMPLEX_H
+#define SPY_COMPLEX_H
+
+#include "spy.h"
+#include "spy/debug.h"
+
+typedef struct {
+    double real;
+    double imag;
+} spy_Complex128;
+#endif /* SPY_COMPLEX_H */
