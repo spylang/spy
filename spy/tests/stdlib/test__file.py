135d989540911aae7945819004096b056c352e82
Antonio Cuni <anto.cuni@gmail.com>
2026-04-02 16:22:44 +0200

    implement readline

--- spy/tests/stdlib/test__file.py
+++ spy/tests/stdlib/test__file.py
@@ -28,27 +28,46 @@ class TestFile(CompilerTest):
         src = """
         from _file import open
 
-        def foo(fname: str) -> tuple[str, str]:
+        def do_read(fname: str) -> tuple[str, str]:
             f = open(fname)
             a = f.read(5)
             b = f.read()
             f.close()
             return a, b
-        """
-        mod = self.compile(src)
-        f = self.write_file("foo.txt", "hello world")
-        assert mod.foo(str(f)) == ("hello", " world")
 
-    def test_read_closed(self):
-        src = """
-        from _file import open
-
-        def foo(fname: str) -> str:
+        def do_read_closed(fname: str) -> str:
             f = open(fname)
             f.close()
             return f.read()
         """
         mod = self.compile(src)
-        f = self.write_file("foo.txt", "hello")
+        f = self.write_file("foo.txt", "hello world")
+        tup = mod.do_read(str(f))
+        assert tup == ("hello", " world")
+
         with SPyError.raises("W_ValueError", match="I/O operation on closed file"):
-            mod.foo(str(f))
+            mod.do_read_closed(str(f))
+
+    def test_readline(self):
+        src = """
+        from _file import open
+
+        def do_readline(fname: str) -> tuple[str, str, str]:
+            f = open(fname)
+            a = f.readline()
+            b = f.readline()
+            c = f.readline()
+            f.close()
+            return a, b, c
+
+        def do_readline_closed(fname: str) -> str:
+            f = open(fname)
+            f.close()
+            return f.readline()
+        """
+        mod = self.compile(src)
+        f = self.write_file("foo.txt", "hello\nworld\n")
+        assert mod.do_readline(str(f)) == ("hello\n", "world\n", "")
+
+        with SPyError.raises("W_ValueError", match="I/O operation on closed file"):
+            mod.do_readline_closed(str(f))
