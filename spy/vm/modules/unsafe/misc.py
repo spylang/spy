1dcd5cece1a36580f1e3c8955e7608812ae68910
Antonio Cuni <anto.cuni@gmail.com>
2026-04-02 09:24:16 +0200

    WIP

--- spy/vm/modules/unsafe/misc.py
+++ spy/vm/modules/unsafe/misc.py
@@ -4,6 +4,7 @@ from spy.vm.object import W_Type
 
 
 def sizeof(w_T: W_Type) -> int:
+    from spy.vm.modules.posix import POSIX
     from spy.vm.modules.unsafe.ptr import W_PtrType
     from spy.vm.struct import W_StructType
 
@@ -22,5 +23,7 @@ def sizeof(w_T: W_Type) -> int:
         # but for native it might be 8. Does it mean that we need to
         # preemptively choose the target platform BEFORE redshifting?
         return 4 + 4  # in debug mode we store both addr and length
+    elif w_T is POSIX.w__FILE:
+        return 4  # XXX
     else:
         raise WIP(f"sizeof({w_T}) not implemented")
