a3fd3ed09f64e6777a3aa4d1679faab66c97d238
paugier <pierre.augier@univ-grenoble-alpes.fr>
2026-05-01 01:27:10 +0200

    examples/jsffi: update raw_c_demo.c

--- examples/jsffi/raw_c_demo.c
+++ examples/jsffi/raw_c_demo.c
@@ -2,62 +2,56 @@
 #include <emscripten.h>
 #include "spy.h"
 
+static spy_Str str_log           = {3,  0, "log"};
+static spy_Str str_getElementById = {14, 0, "getElementById"};
+static spy_Str str_title         = {5,  0, "title"};
+static spy_Str str_btn           = {3,  0, "btn"};
+static spy_Str str_innerText     = {9,  0, "innerText"};
+static spy_Str str_onclick       = {7,  0, "onclick"};
+static spy_Str str_hello_c       = {14, 0, "hello from c 2"};
+static spy_Str str_hello_html    = {18, 0, "hello HTML from C"};
+static spy_Str str_document      = {8,  0, "document"};
+
 EMSCRIPTEN_KEEPALIVE
 void onclick() {
-    printf("onclick!\n");
+    spy_jsffi$js_call_method_1(
+        spy_jsffi$get_Console(), &str_log,
+        spy_jsffi$jsval_from_str(&str_onclick));
 }
-
 
 EMSCRIPTEN_KEEPALIVE
 int main(void) {
-  jsffi_init();
-  JsRef jsffi_GLOBALTHIS = spy_jsffi$get_GlobalThis();
-  JsRef jsffi_CONSOLE = spy_jsffi$get_Console();
+    jsffi_init();
+    JsRef console     = spy_jsffi$get_Console();
+    JsRef globalThis  = spy_jsffi$get_GlobalThis();
 
-  JsRef js_msg = jsffi_string("hello from c 2");
-  jsffi_call_method_1(
-      jsffi_CONSOLE,
-      "log",
-      js_msg);
+    // console.log("hello from c 2")
+    spy_jsffi$js_call_method_1(
+        console, &str_log,
+        spy_jsffi$jsval_from_str(&str_hello_c));
 
-  JsRef js_document = jsffi_getattr(
-      jsffi_GLOBALTHIS,
-      "document"
-  );
+    // document = globalThis.document
+    JsRef document = spy_jsffi$JsRef$__getattribute__(globalThis, &str_document);
 
-  JsRef js_title = jsffi_call_method_1(
-      js_document,
-      "getElementById",
-      jsffi_string("title")
-  );
+    // title = document.getElementById("title")
+    JsRef title = spy_jsffi$js_call_method_1(
+        document, &str_getElementById,
+        spy_jsffi$jsval_from_str(&str_title));
 
-  jsffi_setattr(
-      js_title,
-      "innerText",
-      jsffi_string("hello HTML from C")
-  );
+    // title.innerText = "hello HTML from C"
+    spy_jsffi$JsRef$__setattr__(
+        title, &str_innerText,
+        spy_jsffi$jsval_from_str(&str_hello_html));
 
-  JsRef js_btn = jsffi_call_method_1(
-      js_document,
-      "getElementById",
-      jsffi_string("btn")
-  );
+    // btn = document.getElementById("btn")
+    JsRef btn = spy_jsffi$js_call_method_1(
+        document, &str_getElementById,
+        spy_jsffi$jsval_from_str(&str_btn));
 
-  JsRef js_onclick = jsffi_wrap_func(&onclick);
+    // btn.onclick = onclick
+    spy_jsffi$JsRef$__setattr__(
+        btn, &str_onclick,
+        spy_jsffi$jsval_from_func(onclick));
 
-  jsffi_call_method_1(
-      jsffi_CONSOLE,
-      "log",
-      js_onclick
-  );
-
-  jsffi_setattr(
-      js_btn,
-      "onclick",
-      js_onclick
-  );
-
-  //jsffi_call_method_1(jsffi_CONSOLE, "log", js_div);
-
-  return 0;
+    return 0;
 }
