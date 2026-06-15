ae1662de40177e58935279770c3ed6dc3b13afb6
paugier <pierre.augier@univ-grenoble-alpes.fr>
2026-06-15 03:01:12 +0200

    benchmarks/array_add: inplace add

--- benchmarks/array_add/bench_numpy.py
+++ benchmarks/array_add/bench_numpy.py
@@ -10,3 +10,8 @@ t_start = time()
 for _ in range(100):
     res = arr + arr
 print(f"numpy: {time() - t_start:.3f} s")
+
+t_start = time()
+for _ in range(100):
+    np.add(arr, arr, out=arr)
+print(f"numpy (inplace): {time() - t_start:.3f} s")
