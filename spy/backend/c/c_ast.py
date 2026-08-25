8f96bc497dc82132326e85af2660a362b4b858d4
Pierre Augier <pierre.augier@univ-grenoble-alpes.fr>
2026-08-25 17:28:15 +0200

    SIMD C backend

--- spy/backend/c/c_ast.py
+++ spy/backend/c/c_ast.py
@@ -333,3 +333,18 @@ class Cast(Expr):
 
     def __str__(self) -> str:
         return f"({self.type}){self.expr}"
+
+
+@dataclass
+class Index(Expr):
+    expr: Expr
+    index: Expr
+
+    def precedence(self) -> int:
+        return 14
+
+    def __str__(self) -> str:
+        e = str(self.expr)
+        if self.expr.precedence() < self.precedence():
+            e = f"({e})"
+        return f"{e}[{self.index}]"
