fcc8debba0a47340578fed8f490a061df7af243f
Antonio Cuni <anto.cuni@gmail.com>
2026-09-25 16:14:17 +0200

    merge two tests into one, run the slow part only in C

--- spy/tests/compiler/test_str.py
+++ spy/tests/compiler/test_str.py
@@ -486,21 +486,19 @@ class TestStr(CompilerTest):
         assert mod.strip_chars("hello", "xyz") == "hello"
         assert mod.strip_chars("aaaa", "a") == ""
 
-    def test_strip_chars_bloom(self):
-        src = """
-        def strip_chars(s: str, chars: str) -> str:
-            return s.strip(chars)
-        """
-        mod = self.compile(src)
+        # ==== bloom-filter tests ====
         # false positives must be rejected by the linear scan
         assert mod.strip_chars("!!a!!", "!") == "a"
         assert mod.strip_chars("a!a", "!") == "a!a"
         assert mod.strip_chars("!!ab!!", "!") == "ab"
-        # a very long chars set test case
-        big_chars = "".join(chr(i) for i in range(128) if chr(i) not in "hello")
-        assert mod.strip_chars("xxhello" + big_chars, "xx" + big_chars) == "hello"
-        assert mod.strip_chars(big_chars + "hello" + big_chars, big_chars) == "hello"
-        assert mod.strip_chars("hello", big_chars) == "hello"
+
+        # a very long chars set test case: this is very slow in interp, let's test it
+        # only in C mode
+        if self.backend == "C":
+            big = "".join(chr(i) for i in range(128) if chr(i) not in "hello")
+            assert mod.strip_chars("xxhello" + big, "xx" + big) == "hello"
+            assert mod.strip_chars(big + "hello" + big, big) == "hello"
+            assert mod.strip_chars("hello", big) == "hello"
 
     def test_str_replace(self):
         mod = self.compile("""
